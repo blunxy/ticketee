6536097a96f849cf2f1c02082faae6f8bfaa8b2b
Jordan (Mac) <blunxy@gmail.com>
2011-11-22 22:03:57 -0700

    Added ability to view a list of all projects

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -2,6 +2,10 @@ Given /^I am on the homepage$/ do
   visit('/')
 end
 
+When /^I follow "([^"]*)"$/ do |link_name|
+  click_link(link_name)
+end
+
 When /^I navigate to the new project creation page$/ do
   click_link('New Project')
 end
@@ -20,10 +24,17 @@ Then /^I should be on the newly created project page$/ do
     visit(new_project_path)
 end
 
+Then /^I should be on the project page for "([^"]*)"$/ do |name|
+  visit(project_path(Project.find_by_name!(name)))
+end
+
 Then /^I should see the new project's title$/ do
   page.should have_content("TextMate 2 - Projects - Ticketee")
 end
 
+Given /^there is a project called "([^"]*)"$/ do |name|
+ Factory(:project, :name => name) 
+end
 When /^I create a project without a name$/ do
   click_button('Create Project')
 end
